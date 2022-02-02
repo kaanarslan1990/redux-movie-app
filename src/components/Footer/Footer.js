f5db9e9e3ee467a8a8efa86b515bf56acd5725dd
kaanarslan1990 <oguzkaanarslan@outlook.com>
2022-02-02 16:33:38 +0300

    add shows section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className="footer">
             <div>Movie App</div>
-            <div>©2021, Movie, Inc. or its affiliates</div>
+            <div>©2022, <a href="https://www.linkedin.com/in/arslan-oguzkaan/" target="_blank" rel="noreferrer">Khan&Lion</a> Movie, Inc. or its affiliates</div>
             
         </div>
     );
